58b4efa4b47ae387b0a363b98b8a30cdb12bbc83
Shokal <shokal.ash21016@gmail.com>
2020-12-16 23:36:00 +0600

    2nd

---
+++ FinalTerm/AgroGlow/resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+
+
+<div class="container">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Admin Dashboard</div>
+
+        </div>
+    </div>
+</div>
+@endcontent
